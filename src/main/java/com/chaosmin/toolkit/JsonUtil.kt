7aec81e1aa7228aab59ca4c9ea4621a53dfbfa82
romani <minchao1029@hotmail.com>
2019-07-26 17:19:32 +0800

    ✨ add JsonUtil

---
+++ src/main/java/com/chaosmin/toolkit/JsonUtil.kt
@@ -0,0 +1,170 @@
+package com.chaosmin.toolkit
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.node.*
+import com.fasterxml.jackson.module.kotlin.convertValue
+import com.fasterxml.jackson.module.kotlin.readValue
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Json Toolkit based on Jackson
+ *
+ * @author romani
+ * @since 2019-07-26 15:55
+ */
+object JsonUtil {
+    val objectMapper = ObjectMapper()
+
+    init {
+        resetConfigure()
+    }
+
+    /**
+     * reset configure of objectMapper
+     */
+    fun resetConfigure(): ObjectMapper {
+        return this.objectMapper.apply {
+            findAndRegisterModules()
+            // allow Java style annotation
+            configure(JsonParser.Feature.ALLOW_COMMENTS, true)
+            // allow Yaml style annotation
+            configure(JsonParser.Feature.ALLOW_YAML_COMMENTS, true)
+            // allow single quotes
+            configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true)
+            // allow end commas
+            configure(JsonParser.Feature.ALLOW_TRAILING_COMMA, true)
+            // formatted output
+            configure(SerializationFeature.INDENT_OUTPUT, false)
+            dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA)
+        }
+    }
+
+    /**
+     * see [ObjectMapper.configure]
+     */
+    fun configure(f: JsonParser.Feature, state: Boolean): ObjectMapper {
+        return this.objectMapper.configure(f, state)
+    }
+
+    /**
+     * see [ObjectMapper.configure]
+     */
+    fun configure(f: SerializationFeature, state: Boolean): ObjectMapper {
+        return this.objectMapper.configure(f, state)
+    }
+
+    /**
+     * format object to string
+     * @param obj object need to be formatted
+     * @param prettyPrinter format string in pretty printer
+     */
+    fun encode(obj: Any?, prettyPrinter: Boolean = false): String {
+        return if (obj == null) ""
+        else try {
+            if (!prettyPrinter) objectMapper.writeValueAsString(obj)
+            else objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    /**
+     * parse string to object
+     * @param json string need to be parsed
+     * @param T javaType of object
+     */
+    inline fun <reified T> decode(json: String?): T? {
+        if (json.isNullOrBlank()) return null
+        return try {
+            objectMapper.readValue<T>(json)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    /**
+     * parse string to list
+     * @param json string need to be parsed
+     * @param T javaType of element in list
+     */
+    inline fun <reified T> parseToList(json: String?): List<T> {
+        return if (json.isNullOrBlank()) emptyList()
+        else {
+            val javaType = objectMapper.typeFactory.constructParametricType(List::class.java, T::class.java)
+            objectMapper.readValue(json, javaType)
+        }
+    }
+
+    /**
+     * parse string to map
+     * @param json string need to be parsed
+     * @param K javaType of key in map
+     * @param V javaType of value in map
+     */
+    inline fun <reified K, reified V> parseToMap(json: String?): Map<K, V> {
+        return if (json.isNullOrBlank()) emptyMap()
+        else {
+            val javaType = objectMapper.typeFactory.constructParametricType(Map::class.java, K::class.java, V::class.java)
+            objectMapper.readValue(json, javaType)
+        }
+    }
+
+    /**
+     * convert [JsonNode]'s value with judgment of node type, if node type is [MissingNode] then return null
+     * @param node jsonNode need to be converted
+     */
+    inline fun <reified T> convertValue(node: JsonNode): T? {
+        return if (node is MissingNode) null
+        else objectMapper.convertValue<T>(node)
+    }
+
+    /**
+     * create basic JsonNode within designated JavaType
+     */
+    private fun <T> basicNode(value: T): JsonNode {
+        return when (value) {
+            is String -> TextNode(value)
+            is Int -> IntNode(value)
+            is Long -> LongNode(value)
+            is Double -> DoubleNode(value)
+            is Boolean -> BooleanNode.valueOf(value)
+            else -> POJONode(value)
+        }
+    }
+
+    /**
+     * create a new JsonNode by ObjectMapper
+     */
+    fun newNode(): JsonNode {
+        return objectMapper.createObjectNode()
+    }
+
+    /**
+     * create a new ArrayNode by ObjectMapper
+     */
+    fun newArrayNode(): JsonNode {
+        return objectMapper.createArrayNode()
+    }
+
+    /**
+     * create a new JsonNode with key and value by ObjectMapper
+     * @param key key of node
+     * @param value value of node
+     */
+    fun <T> newNode(key: String, value: T): JsonNode {
+        return objectMapper.createObjectNode().set(key, basicNode(value))
+    }
+
+    /**
+     * create a new ArrayNode with key and value by ObjectMapper
+     * @param key key of node
+     * @param value value of node(must be list)
+     */
+    fun <T> newArrayNode(key: String, value: List<T>): JsonNode {
+        return objectMapper.createArrayNode().addAll(value.mapNotNull { basicNode(it) })
+    }
+}
---
+++ src/test/java/com/chaosmin/toolkit/JsonUtilTest.kt
@@ -0,0 +1,103 @@
+package com.chaosmin.toolkit
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.node.TextNode
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+
+/**
+ * @author romani
+ * @since 2019-07-26 16:22
+ */
+class JsonUtilTest {
+
+    class TestClass {
+        var name: String? = null
+        var age: Int? = null
+    }
+
+    @Test
+    fun resetConfigure() {
+        val obj = TestClass().apply {
+            this.name = "romani"
+            this.age = 24
+        }
+        assertEquals("""{"name":"romani","age":24}""", JsonUtil.encode(obj))
+        JsonUtil.configure(SerializationFeature.INDENT_OUTPUT, true)
+        JsonUtil.configure(JsonParser.Feature.ALLOW_TRAILING_COMMA, false)
+        assertEquals("{\n  \"name\" : \"romani\",\n  \"age\" : 24\n}", JsonUtil.encode(obj))
+        JsonUtil.resetConfigure()
+        assertEquals("""{"name":"romani","age":24}""", JsonUtil.encode(obj))
+    }
+
+    @Test
+    fun encode() {
+        val obj = TestClass().apply {
+            this.name = "romani"
+            this.age = 24
+        }
+        assertEquals("""{"name":"romani","age":24}""", JsonUtil.encode(obj))
+        assertEquals("{\n  \"name\" : \"romani\",\n  \"age\" : 24\n}", JsonUtil.encode(obj, true))
+    }
+
+    @Test
+    fun decode() {
+        val obj = JsonUtil.decode<TestClass>("""{"name":"romani","age":24}""")
+        assertEquals("romani", obj?.name)
+        val list = JsonUtil.decode<List<TestClass>>("""[{"name":"romani","age":24}]""")
+        assertEquals("romani", list?.firstOrNull()?.name)
+    }
+
+    @Test
+    fun parseToList() {
+        val listEmp = JsonUtil.parseToList<TestClass>("")
+        assertEquals(0, listEmp.size)
+
+        val list = JsonUtil.parseToList<TestClass>("""[{"name":"romani","age":24}]""")
+        assertEquals("romani", list.firstOrNull()?.name)
+
+        val lists = JsonUtil.parseToList<List<String>>("""[["1"],["2","3"],["4"]]""")
+        assertEquals(3, lists.size)
+        assertEquals(2, lists[1].size)
+    }
+
+    @Test
+    fun parseToMap() {
+        val mapEmp = JsonUtil.parseToMap<String, String>("")
+        assertEquals(0, mapEmp.size)
+
+        val mapSS = JsonUtil.parseToMap<String, String>("""{"1":"1"}""")
+        assertEquals("1", mapSS.keys.firstOrNull())
+        assertEquals("1", mapSS.values.firstOrNull())
+
+        val mapSI = JsonUtil.parseToMap<String, Int>("""{"1":"1"}""")
+        assertEquals("1", mapSI.keys.firstOrNull())
+        assertEquals(1, mapSI.values.firstOrNull())
+
+        val maps = JsonUtil.parseToMap<String, Map<String, String>>("""{"name":{"first":"min","second":"chao"}}""")
+        assertEquals("chao", maps["name"]?.get("second"))
+    }
+
+    @Test
+    fun newNode() {
+        assertEquals(ObjectNode::class.java, JsonUtil.newNode().javaClass)
+        assertEquals(TextNode::class.java, JsonUtil.newNode("name", "romani")["name"].javaClass)
+    }
+
+    @Test
+    fun newArrayNode() {
+        assertEquals(0, JsonUtil.newArrayNode().size())
+        assertEquals(3, JsonUtil.newArrayNode("name", listOf("ro", "ma", "ni")).size())
+    }
+
+    @Test
+    fun convertValue() {
+        val missingNode = JsonUtil.newNode().path("notExists")
+        assertNull(JsonUtil.convertValue<Any>(missingNode))
+        val objNode = JsonUtil.newNode("name", "romani").path("name")
+        assertEquals("romani", JsonUtil.convertValue(objNode))
+    }
+}
